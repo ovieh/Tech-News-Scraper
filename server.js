0f0022cd006081e3cf43034f3a7b87551c2d238a
ovieh <ovieh.mosley@gmail.com>
2017-12-24 11:35:04 -0500

    connected to database

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
-const mongoose = require("mongoose")
+const logger = require('morgan');
 
 const PORT = process.env.PORT || 3000;
+
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongonews';
+
+const db = mongoose.createConnection(DB_URI, {
+    useMongoClient: true
+});
+
+db.on('error', console.error.bind('console','MongoDB conneciton error'));
+db.once('open', () => console.log('connected to database'));
+
+app.use(logger('dev'));
+
 
 // Linking routes
 require("./routes/html-routes")(app);
