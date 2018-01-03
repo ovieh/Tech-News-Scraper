99ba4bb650b48fa0b22d1455d18adc18c81e6a75
Ovieh <ovieh.mosley@gmail.com>
2018-01-03 14:55:50 -0500

    working on scraping into database

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,9 +4,23 @@ const Comments = require("../models/comments");
 const scraper = require('../controller/scraper');
 
 module.exports = app => {
-    scraper();
-    app.get("/article/scrape", (req, res) => {
+
+    app.get("/api/scrape", (req, res) => {
         // res.json(articles)
-        res.json({test: "blaj"});
+        scraper()
+        .then( data => {
+            Article.create(data, (err, newArticles) => {
+                if (err) console.log(err.message);
+                res.json(newArticles);
+            })
+        })
+        .catch( err => res.status(400).send(err) );
+    });
+
+    app.get("/api/articles", (req, res) => {
+        Article.find({})
+            .then( articles => res.json(articles) )
+            .catch(err => res.status(500).json(err));
     });
 }
+
