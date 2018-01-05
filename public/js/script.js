02547d9b26587421e6ca03ecdf4def83bb7dc809
Ovieh <ovieh.mosley@gmail.com>
2018-01-05 17:32:15 -0500

    dummy comment displays on click

--- public/js/script.js
+++ public/js/script.js
@@ -1,86 +1,90 @@
-function scrapeArticles() {
-	$.get('/scrape')
-		.done(function (data, status, response) {
+(function () {
+	function scrapeArticles() {
+		$.get('/scrape')
+			.done(function (data, status, response) {
 
-			let modalBody = $('#resultsModal').find('.modal-card-body');
+				let modalBody = $('#resultsModal').find('.modal-card-body');
 
-			if (response.status === 200) {
+				if (response.status === 200) {
 
-				modalBody.text(`${data.length} articles were added!`);
-				// console.log(`${data.length} articles were added!`);
-			}
-			$('#resultsModal').addClass('is-active');
+					modalBody.text(`${data.length} articles were added!`);
+					// console.log(`${data.length} articles were added!`);
+				}
+				$('#resultsModal').addClass('is-active');
 
-		})
-		.fail(function (error) {
-			console.log('Looks like there was a problem: \n', error);
+			})
+			.fail(function (error) {
+				console.log('Looks like there was a problem: \n', error);
+			});
+	}
+
+	function saveArticle(event) {
+		const articleId = $(event.target).attr('data-id');
+		const data = {
+			id: articleId
+		};
+
+		$.ajax({
+			method: 'POST',
+			url: '/articles/save',
+			data: data
+		}).done(function (article, status, response) {
+			if (response.status === 200) window.location.reload();
+			else console.log(`unexpected response. response status ${response.status}`);
+		}).fail(function (response) {
+			console.log('failed to save article', response.status);
 		});
-}
 
-function saveArticle(event) {
-	const articleId = $(event.target).attr('data-id');
-	const data = {
-		id: articleId
-	};
+	}
 
-	$.ajax({
-		method: 'POST',
-		url: '/articles/save',
-		data: data
-	}).done(function (article, status, response) {
-		if (response.status === 200) window.location.reload();
-		else console.log(`unexpected response. response status ${response.status}`);
-	}).fail(function (response) {
-		console.log('failed to save article', response.status);
-	});
+	function unSaveArticle(event) {
+		const articleId = $(event.target).attr('data-id');
+		const data = {
+			id: articleId
+		};
 
-}
+		$.ajax({
+			method: 'POST',
+			url: '/articles/unsave',
+			data: data
+		}).done(function (article, status, response) {
+			if (response.status === 200) window.location.reload();
+			else console.log(`unexpected response. response status ${response.status}`);
+		}).fail(function (response) {
+			console.log(`failed to unsave article, response status ${response.status}`);
+		});
 
-function unSaveArticle(event) {
-	const articleId = $(event.target).attr('data-id');
-	const data = {
-		id: articleId
-	};
+	}
 
-	$.ajax({
-		method: 'POST',
-		url: '/articles/unsave',
-		data: data
-	}).done(function (article, status, response) {
-		if (response.status === 200) window.location.reload();
-		else console.log(`unexpected response. response status ${response.status}`);
-	}).fail(function (response) {
-		console.log(`failed to unsave article, response status ${response.status}`);
-	});
-
-}
+	function showComments() {
+		$('#commentsModal').addClass('is-active');
+	}
 
 
 
 
 
 
+	function pageReady() {
+		// document.getElementById('scrape').onclick = scrapeArticles;
+		$('#scrape').on('click', scrapeArticles);
+		$('.save').on('click', saveArticle);
+		$('.unsave').on('click', unSaveArticle);
+		$('.comments').on('click', showComments);
+
+		//Results modal
+		$('.modal-close').click(function () {
+			$('#resultsModal').removeClass('is-active');
+			window.location.href = '/';
+		});
+		//Comments modal
+		$('.delete').click(function () {
+			$('#commentsModal').removeClass('is-active');
+			window.location.reload();
+		});
 
 
-function pageReady() {
-	// document.getElementById('scrape').onclick = scrapeArticles;
-	$('#scrape').on('click', scrapeArticles);
-	$('.save').on('click', saveArticle);
-	$('.unsave').on('click', unSaveArticle);
+	}
 
-
-	//Results modal
-	$('.modal-close').click(function () {
-		$('#resultsModal').removeClass('is-active');
-		window.location.href = '/';
-	});
-	//Results modal
-	$('.modal-close').click(function () {
-		$('#commentsModal').removeClass('is-active');
-		window.location.href = '/';
-	});
-
-
-}
-
-document.addEventListener('DOMContentLoaded', pageReady);
+	document.addEventListener('DOMContentLoaded', pageReady);
+})();
