8cf8970bc23a54de576f11003fc4a72953652681
Ovieh <ovieh.mosley@gmail.com>
2018-01-04 21:33:41 -0500

    still working on delployment

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(DB_URI);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind('console','MongoDB connection error'));
-db.once('open', () => console.log('connected to database'));
+// db.once('open', () => console.log('connected to database'));
 
 app.use(logger('dev'));
 
